6dd8a0a72a1176d0c6beca8a975a481ec8d20c91
YiyiF <yiyifu97@gmail.com>
2018-07-06 22:29:55 +0800

    folder_test

---
+++ test_1/1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  6 21:52:19 2018
+
+@author: eleve
+"""
+
+print("Hello, Github;)")
---
+++ test_2/t2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  6 21:56:11 2018
+
+@author: eleve
+"""
+
+print("Way of uploading folders through Git.")
